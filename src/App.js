db8e0d0bb938522d84e9c4d3e602568cfae5bb64
Abriel Cleaver <abrielcleaver@gmail.com>
2022-01-27 12:38:49 -0700

    Add onClick to Alien monster button fo DECREASING size & decreases as expected

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
             {/* when you click this button, the alien's size in state should go up by one */}
             <button onClick={() => setAlienSize(alienSize + 1)}>Oh no! The alien is gobbling up all the electricity!</button>
             {/* when you click this button, the lizard's size in state should go down by one */}
-            <button >Amazing! The alien zapped the lizard!</button>
+            <button onClick={() => setAlienSize(alienSize - 1)}>Amazing! The alien zapped the lizard!</button>
           </div>
         </div>
         <div className="monster">
