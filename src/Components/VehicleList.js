d20d7672dbecc1f022f9dd00f02d3f2080cbdcf1
Abriel Cleaver <abrielcleaver@gmail.com>
2022-01-26 19:47:31 -0700

    fixed typo

--- src/Components/VehicleList.js
+++ src/Components/VehicleList.js
@@ -5,7 +5,7 @@ export default function VehicleList() {
     <div className="traffic">
       {/* this component takes in a prop called vehicles, which is an array of vehicle strings */}
       {/* map through the vehicles array in props */}
-      {/* for each item render a Vehicle component. Pass the vehicl string as a prop called vehicle to the Vehicle component.  */}
+      {/* for each item render a Vehicle component. Pass the vehicle string as a prop called vehicle to the Vehicle component.  */}
     </div>
   );
 }
